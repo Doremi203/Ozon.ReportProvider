3096169115bcb72e2f6466022f1a1b0354b3e3b9
Doremi203 <maxim.r.ivanov@gmail.com>
2024-04-25 23:28:58 +0300

    Added Kafka Background Service

---
+++ src/Ozon.ReportProvider.Api/KafkaBackgroundService.cs
@@ -0,0 +1,42 @@
+using Confluent.Kafka;
+using Microsoft.Extensions.Options;
+using Ozon.ReportProvider.Kafka;
+using Ozon.ReportProvider.Kafka.Config;
+
+namespace Ozon.ReportProvider.Api;
+
+public class KafkaBackgroundService<TKey, TValue, THandler>(
+    IOptions<KafkaSettings> settings,
+    IServiceProvider serviceProvider,
+    ILogger<KafkaBackgroundService<TKey, TValue, THandler>> logger,
+    IDeserializer<TKey>? keyDeserializer,
+    IDeserializer<TValue>? valueDeserializer
+) : BackgroundService where THandler : IHandler<TKey, TValue>
+{
+    private readonly KafkaConsumer<TKey, TValue> _consumer = new(
+        settings,
+        serviceProvider.GetRequiredService<ILogger<KafkaConsumer<TKey, TValue>>>(),
+        serviceProvider.GetRequiredService<THandler>(),
+        keyDeserializer,
+        valueDeserializer
+    );
+
+    public override Task StopAsync(CancellationToken cancellationToken)
+    {
+        _consumer.Dispose();
+
+        return Task.CompletedTask;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        try
+        {
+            await _consumer.Consume(stoppingToken);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Unhandled exception occured");
+        }
+    }
+}
