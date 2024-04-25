45b162a39c009751245acc0094cf86c5d72a81e6
Doremi203 <maxim.r.ivanov@gmail.com>
2024-04-25 15:31:00 +0300

    Added grpc to api project

--- src/Ozon.ReportProvider.Api/Program.cs
+++ src/Ozon.ReportProvider.Api/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
