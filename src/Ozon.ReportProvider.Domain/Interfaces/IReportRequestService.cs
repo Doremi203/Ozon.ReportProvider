bd6220b966b0cd554059960d5df4544fc5a548b4
Doremi203 <maxim.r.ivanov@gmail.com>
2024-04-26 14:07:09 +0300

    Added cancellation token to report request service

--- src/Ozon.ReportProvider.Domain/Interfaces/IReportRequestService.cs
+++ src/Ozon.ReportProvider.Domain/Interfaces/IReportRequestService.cs
@@ -4,5 +4,5 @@ namespace Ozon.ReportProvider.Domain.Interfaces;
 
 public interface IReportRequestService
 {
-    Task ProcessReportRequests(ReportRequestEvent[] reportRequestEvents);
+    Task ProcessReportRequests(ReportRequestEvent[] reportRequestEvents, CancellationToken cancellationToken);
 }
