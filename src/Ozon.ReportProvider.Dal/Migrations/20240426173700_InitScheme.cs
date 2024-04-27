d46c9e6aacd4444406a977e2d8bcd3b03e2075ef
Doremi203 <maxim.r.ivanov@gmail.com>
2024-04-27 18:47:00 +0300

    Changed migrations to respect contract

--- src/Ozon.ReportProvider.Dal/Migrations/20240426173700_InitScheme.cs
+++ src/Ozon.ReportProvider.Dal/Migrations/20240426173700_InitScheme.cs
@@ -7,10 +7,9 @@ public class InitScheme : Migration {
     public override void Up()
     {
         Create.Table("report_requests")
-            .WithColumn("id").AsInt64().PrimaryKey("reports_pk").Identity()
+            .WithColumn("request_id").AsGuid().PrimaryKey("report_requests_pk").NotNullable()
             .WithColumn("user_id").AsGuid().NotNullable()
             .WithColumn("good_id").AsGuid().NotNullable()
-            .WithColumn("layout_id").AsInt64().NotNullable()
             .WithColumn("start_of_period").AsDateTimeOffset().NotNullable()
             .WithColumn("end_of_period").AsDateTimeOffset().NotNullable()
             .WithColumn("created_at").AsDateTimeOffset().NotNullable();
