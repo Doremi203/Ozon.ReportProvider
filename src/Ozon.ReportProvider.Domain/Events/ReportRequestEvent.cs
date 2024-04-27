af72100738b0de78267187ffcbbc6876fd7f72c4
Doremi203 <maxim.r.ivanov@gmail.com>
2024-04-27 18:39:44 +0300

    Changed event contract

--- src/Ozon.ReportProvider.Domain/Events/ReportRequestEvent.cs
+++ src/Ozon.ReportProvider.Domain/Events/ReportRequestEvent.cs
@@ -2,8 +2,8 @@ namespace Ozon.ReportProvider.Domain.Events;
 
 public record ReportRequestEvent
 {
+    public required Guid RequestId { get; init; }
+    public required Guid GoodId { get; init; }
     public required DateTimeOffset StartOfPeriod { get; init; }
     public required DateTimeOffset EndOfPeriod { get; init; }
-    public required Guid GoodId { get; init; }
-    public required long LayoutId { get; init; }
 }
