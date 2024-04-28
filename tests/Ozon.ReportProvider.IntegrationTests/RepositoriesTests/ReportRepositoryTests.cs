66a557816b51939bb944386af6280b40854bad9c
Doremi203 <maxim.r.ivanov@gmail.com>
2024-04-28 22:32:52 +0300

    Added tests for report repository

---
+++ tests/Ozon.ReportProvider.IntegrationTests/RepositoriesTests/ReportRepositoryTests.cs
@@ -0,0 +1,42 @@
+using FluentAssertions;
+using Ozon.ReportProvider.Domain.Interfaces.Repositories;
+using Ozon.ReportProvider.IntegrationTests.Fakers;
+using Ozon.ReportProvider.IntegrationTests.Fixtures;
+
+namespace Ozon.ReportProvider.IntegrationTests.RepositoriesTests;
+
+[Collection(nameof(DalTestFixture))]
+public class ReportRepositoryTests(DalTestFixture fixture)
+{
+    private readonly IReportRepository _reportRepository = fixture.ReportRepository;
+    
+    [Fact]
+    public async Task Add_Success()
+    {
+        // Arrange
+        var reports = ReportEntityV1Faker.Generate(5);
+        var ids = reports.Select(x => x.RequestId).ToArray();
+
+        // Act
+        await _reportRepository.Add(reports, CancellationToken.None);
+
+        // Assert
+        var actual = await _reportRepository.GetReports(ids, CancellationToken.None);
+        actual.Should().BeEquivalentTo(reports);
+    }
+    
+    [Fact]
+    public async Task GetReports_Success()
+    {
+        // Arrange
+        var reports = ReportEntityV1Faker.Generate(5);
+        var ids = reports.Select(x => x.RequestId).ToArray();
+        await _reportRepository.Add(reports, CancellationToken.None);
+
+        // Act
+        var actual = await _reportRepository.GetReports(ids, CancellationToken.None);
+
+        // Assert
+        actual.Should().BeEquivalentTo(reports);
+    }
+}
