bb26f636a442e6eedf7035c68d7b1d2c0cd87f4f
Doremi203 <maxim.r.ivanov@gmail.com>
2024-04-28 22:59:57 +0300

    Fixed get report model. Added unit tests for report request service

---
+++ tests/Ozon.ReportProvider.UnitTests/ReportRequestServiceTests.cs
@@ -0,0 +1,57 @@
+using AutoBogus;
+using Mapster;
+using Moq;
+using Ozon.ReportProvider.Bll.Services;
+using Ozon.ReportProvider.Domain.Events;
+using Ozon.ReportProvider.Domain.Interfaces.Services;
+using Ozon.ReportProvider.Domain.Models;
+
+namespace Ozon.ReportProvider.UnitTests;
+
+public class ReportRequestServiceTests
+{
+    private readonly Mock<IApiReportService> _apiReportServiceFake;
+    private readonly Mock<IReportService> _reportServiceFake;
+    private readonly IReportRequestService _reportRequestService;
+
+    public ReportRequestServiceTests()
+    {
+        _apiReportServiceFake = new(MockBehavior.Strict);
+        _reportServiceFake = new(MockBehavior.Strict);
+        _reportRequestService = new ReportRequestService(
+            _apiReportServiceFake.Object, _reportServiceFake.Object);
+    }
+
+    [Fact]
+    public async Task ProcessReportRequests_Success()
+    {
+        // Arrange
+        var reportRequestEvents = new AutoFaker<ReportRequestEvent>()
+            .Generate(3).ToArray();
+        var getReportModels = reportRequestEvents.Adapt<GetReportModel[]>();
+        var reports = new AutoFaker<Report>()
+            .Generate(3).ToArray();
+
+        _apiReportServiceFake
+            .Setup(x => x.GetReports(It.IsAny<GetReportModel[]>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(reports);
+        _reportServiceFake
+            .Setup(x => x.StoreReports(It.IsAny<Report[]>(), It.IsAny<CancellationToken>()))
+            .Returns(Task.CompletedTask);
+
+        // Act
+        await _reportRequestService.ProcessReportRequests(reportRequestEvents, CancellationToken.None);
+
+        // Assert
+        _apiReportServiceFake.Verify(x =>
+                x.GetReports(It.Is<GetReportModel[]>(m =>
+                        m.SequenceEqual(getReportModels)),
+                    It.IsAny<CancellationToken>()),
+            Times.Once);
+        _reportServiceFake.Verify(x =>
+                x.StoreReports(It.Is<Report[]>(r =>
+                        r.SequenceEqual(reports)),
+                    It.IsAny<CancellationToken>()),
+            Times.Once);
+    }
+}
