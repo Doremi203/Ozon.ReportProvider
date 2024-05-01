bc29b05f5f0d6419a76983f6d58aa0938ac6e400
Doremi203 <maxim.r.ivanov@gmail.com>
2024-05-01 12:54:11 +0300

    Added more report request service tests

--- tests/Ozon.ReportProvider.UnitTests/ReportRequestServiceTests.cs
+++ tests/Ozon.ReportProvider.UnitTests/ReportRequestServiceTests.cs
@@ -23,7 +23,7 @@ public class ReportRequestServiceTests
     }
 
     [Fact]
-    public async Task ProcessReportRequests_Success()
+    public async Task ProcessReportRequests_AllRequestsUncomplete_Success()
     {
         // Arrange
         var reportRequestEvents = new AutoFaker<ReportRequestEvent>()
@@ -54,6 +54,80 @@ public class ReportRequestServiceTests
         _reportServiceFake.Verify(x =>
                 x.StoreReports(It.Is<Report[]>(r =>
                         r.SequenceEqual(reports)),
+                    It.IsAny<CancellationToken>()),
+            Times.Once);
+    }
+
+    [Fact]
+    public async Task ProcessReportRequests_SomeRequestsUncomplete_ShouldProcessUncompleteRequests()
+    {
+        // Arrange
+        var reportRequestEvents = new AutoFaker<ReportRequestEvent>()
+            .Generate(3).ToArray();
+        var uncompleteRequests = reportRequestEvents.Take(2).ToArray();
+        var getReportModels = uncompleteRequests.Adapt<ApiGetReportModel[]>();
+        var reports = new AutoFaker<Report>()
+            .Generate(2).ToArray();
+
+        _apiReportServiceFake
+            .Setup(x => x.GetReports(It.IsAny<ApiGetReportModel[]>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(reports);
+        _reportServiceFake
+            .Setup(x => x.StoreReports(It.IsAny<Report[]>(), It.IsAny<CancellationToken>()))
+            .Returns(Task.CompletedTask);
+        _reportServiceFake
+            .Setup(x => x.GetUncompleteReportRequests(It.IsAny<ReportRequestEvent[]>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(uncompleteRequests);
+
+        // Act
+        await _reportRequestService.ProcessReportRequests(reportRequestEvents, CancellationToken.None);
+
+        // Assert
+        _apiReportServiceFake.Verify(x =>
+                x.GetReports(It.Is<ApiGetReportModel[]>(m =>
+                        m.SequenceEqual(getReportModels)),
+                    It.IsAny<CancellationToken>()),
+            Times.Once);
+        _reportServiceFake.Verify(x =>
+                x.StoreReports(It.Is<Report[]>(r =>
+                        r.SequenceEqual(reports)),
+                    It.IsAny<CancellationToken>()),
+            Times.Once);
+        _reportServiceFake.Verify(x =>
+                x.GetUncompleteReportRequests(
+                    It.IsAny<ReportRequestEvent[]>(),
+                    It.IsAny<CancellationToken>()),
+            Times.Once);
+    }
+    
+    [Fact]
+    public async Task ProcessReportRequests_NoRequestsUncomplete_ShouldSkipProcessing()
+    {
+        // Arrange
+        var reportRequestEvents = new AutoFaker<ReportRequestEvent>()
+            .Generate(3).ToArray();
+        var uncompleteRequests = Array.Empty<ReportRequestEvent>();
+        var getReportModels = uncompleteRequests.Adapt<ApiGetReportModel[]>();
+        
+        _reportServiceFake
+            .Setup(x => x.GetUncompleteReportRequests(It.IsAny<ReportRequestEvent[]>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(uncompleteRequests);
+
+        // Act
+        await _reportRequestService.ProcessReportRequests(reportRequestEvents, CancellationToken.None);
+
+        // Assert
+        _apiReportServiceFake.Verify(x =>
+                x.GetReports(It.IsAny<ApiGetReportModel[]>(),
+                    It.IsAny<CancellationToken>()),
+            Times.Never);
+        _reportServiceFake.Verify(x =>
+                x.StoreReports(It.IsAny<Report[]>(),
+                    It.IsAny<CancellationToken>()),
+            Times.Never);
+        _reportServiceFake.Verify(x =>
+                x.GetUncompleteReportRequests(
+                    It.IsAny<ReportRequestEvent[]>(),
                     It.IsAny<CancellationToken>()),
             Times.Once);
     }
