2af7836e4161aa358923e4475fa85e75e3988947
Doremi203 <maxim.r.ivanov@gmail.com>
2024-05-01 12:33:04 +0300

    Added tests to report repository

--- tests/Ozon.ReportProvider.IntegrationTests/RepositoriesTests/ReportRepositoryTests.cs
+++ tests/Ozon.ReportProvider.IntegrationTests/RepositoriesTests/ReportRepositoryTests.cs
@@ -27,17 +27,48 @@ public class ReportRepositoryTests(DalTestFixture fixture)
     }
     
     [Fact]
-    public async Task GetReports_Success()
+    public async Task GetReport_ReportExists_ReturnsReport()
     {
         // Arrange
-        var reports = ReportEntityV1Faker.Generate(5);
+        var reports = ReportEntityV1Faker.Generate();
         var ids = reports.Select(x => new RequestId(x.RequestId)).ToArray();
         await _reportRepository.Add(reports, CancellationToken.None);
 
         // Act
-        var actual = await _reportRepository.GetReports(ids, CancellationToken.None);
+        var actual = await _reportRepository.GetReport(ids[0], CancellationToken.None);
 
         // Assert
-        actual.Should().BeEquivalentTo(reports);
+        actual.Should().BeEquivalentTo(reports[0]);
+    }
+
+    [Fact]
+    public async Task GetReport_NoReport_ReturnsNull()
+    {
+        // Arrange
+        var id = new RequestId(-1);
+        
+        // Act
+        var actual = await _reportRepository.GetReport(id, CancellationToken.None);
+        
+        // Assert
+        actual.Should().BeNull();
+    }
+
+    [Fact]
+    public async Task GetCompletedRequestIds_Success()
+    {
+        // Arrange
+        var reports = ReportEntityV1Faker.Generate(5);
+        await _reportRepository.Add(reports, CancellationToken.None);
+        var existingIds = reports.Select(x => new RequestId(x.RequestId)).ToArray();
+        var expectedIds = existingIds.Take(2).ToArray();
+        var ids = new List<RequestId> { new(-1), new(-2), new(-3) };
+        ids.AddRange(expectedIds);
+        
+        // Act
+        var actual = await _reportRepository.GetCompletedRequestIds(ids.ToArray(), CancellationToken.None);
+        
+        // Assert
+        actual.Should().BeEquivalentTo(expectedIds);
     }
 }
